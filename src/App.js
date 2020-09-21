a44ffb929a7f87b8ddd3b6c897832742eb61aea3
MomohNobert <okhai0112@gmail.com>
2020-09-21 11:07:40 +0100

    trying another direction, thanks youtube!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Fade from './components/fade/fade.component';
-
 function App() {
-  const [isVisible, setIsVisible] = useState(false)
+
+  const [titleScreen, setTitleScreen] = useState(true);
+  const [gameBoard, setGameBoard] = useState(false);
+
+  const startGame = () => {
+    setTitleScreen(false);
+    setGameBoard(true);
+  }  
+
   return (
-    <div className="App">
-      {
-        isVisible ? <Fade /> : null
-      }
-      <button type="button" onClick={() => setIsVisible(!isVisible)}>
-        Set Visiblity!
-      </button>
+    <div className="game">
+      {titleScreen && <TitleScreen startGame={startGame} />}
+      {gameBoard && <GameBoard />}
     </div>
   );
 }
+
+function TitleScreen({ startGame }) {
+  return (
+    <div className="title-screen">
+      <svg onClick={startGame} className="icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 51.6 51.6" style={{ enableBackground: 'new 0 0 51.6 51.6' }} xmlSpace="preserve">
+        <line className="icon__line" x1="10.4" y1="10" x2="41.2" y2="41.6"/>
+        <line className="icon__line" x1="41.6" y1="10.4" x2="10" y2="41.2"/>
+      </svg>
+      <svg onClick={startGame} className="icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 60 60" style={{ enableBackground: 'new 0 0 60 60' }} xmlSpace="preserve">
+        <circle className="icon__line" cx="30" cy="30" r="20"/>
+      </svg>
+    </div>
+  );
+}
+
+// ----------
+// Game Board
+// ----------
+
+function GameBoard() {
+  return (
+    <div className="game-board">
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+      <button className="game-board__button"></button>
+    </div>
+  );
+}
+
+// ----------
+// Render App
+// ----------
 
 export default App;
