8a4a13f2e9fef6b67362b75b066d11439c5756a6
MomohNobert <okhai0112@gmail.com>
2020-09-22 10:55:48 +0100

    added leaderboard

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import PlayerSwap from './components/player-swap/player-swap.component';
 import Board from './components/game-board/game-board.component';
+import LeaderBoard from './components/leadeboard/leaderboard.component';
 
 function App() {
   return (
     <div className="app">
       <PlayerSwap />
       <Board />
+      <LeaderBoard />
     </div>
   );
 }
